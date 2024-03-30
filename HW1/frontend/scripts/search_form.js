73686269c39239fbeab5a92af895ae1254976e40
P-Ramos16 <p.ramos@ua.pt>
2024-03-30 17:38:05 +0000

    Added HW1 frontend initial page

---
+++ HW1/frontend/scripts/search_form.js
@@ -0,0 +1,14 @@
+function search_form() {
+    alert('Form submitted!');
+    return false;
+}
+
+$('.dropdown-el').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+  });
+  $(document).click(function() {
+    $('.dropdown-el').removeClass('expanded');
+});
